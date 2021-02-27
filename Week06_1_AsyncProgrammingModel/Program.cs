4a61c05bd5019d03dac9dcdfee1681fb4deb74c3
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2021-02-26 22:36:30 -0500

    Week06

---
+++ Week06_1_AsyncProgrammingModel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Week06_1_AsyncProgrammingModel
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // get the path of the app data folder
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+            // create an DirectoryInfo object instance which contains information about the directory
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            // retrieve all the files ending with file extension .txt in the folder and all its subfolders
+            var files = directoryInfo.GetFiles("*.txt", SearchOption.AllDirectories);
+
+            Console.WriteLine($"Found {files.Length} files");
+
+            var buffer = new byte[1024];
+            foreach (var fileInfo in files)
+            {
+                var stream = fileInfo.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
+                stream.BeginRead(buffer, 0, buffer.Length, HandleRead, stream);
+            }
+
+        }
+
+        private static void HandleRead(IAsyncResult result)
+        {
+            var fileStream = (FileStream)result.AsyncState;
+            var bytesRead = fileStream.EndRead(result);
+
+            Console.WriteLine($"Read {bytesRead} bytes from file: {fileStream.Name}");
+        }
+    }
+}
---
+++ Week06_2_AsyncDatabaseAccess/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using Week06_2_AsyncDatabaseAccess.Data;
+using Week06_2_AsyncDatabaseAccess.Models;
+
+namespace Week06_2_AsyncDatabaseAccess
+{
+    class Program
+    {
+        private static AppDbContext db = new AppDbContext();
+        static async Task Main(string[] args)
+        {
+
+            Console.WriteLine("Starting Sync. DB Operation");
+
+            //Save to DB Sync
+            DbSave();
+
+
+            Console.WriteLine("Press enter to continue");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Starting Async. DB Operation");
+
+            //Save to DB Async
+            var task = DbSaveAsync();
+
+            Console.WriteLine("Continue");
+            await task;
+
+
+
+        }
+
+        private static void DbSave()
+        {
+            //Synchronous Database Insert
+            for (int i = 0; i < 100000; i++)
+            {
+                Person newPerson = new Person()
+                {
+                    FirstName = "Sync",
+                    LastName = "Doe",
+                    CreationTime = DateTime.Now
+                };
+
+                db.Persons.Add(newPerson);  //Add the newly object to the queue to be inserted later
+            }
+
+
+            db.SaveChanges();   //New records are not saved in the database unless SaveChanges(), or SaveChangesAsync() is called.
+
+
+        }
+
+        private static async Task DbSaveAsync()
+        {
+
+            //Asynchronous Database Insert
+            for (int i = 0; i < 100000; i++)
+            {
+                Person newPerson = new Person()
+                {
+                    FirstName = "Async",
+                    LastName = "Doe",
+                    CreationTime = DateTime.Now
+                };
+
+                db.Persons.Add(newPerson);  //Add the newly object to the queue to be inserted later
+            }
+
+            Console.WriteLine("before async");
+            await db.SaveChangesAsync();    //New records are not saved in the database unless SaveChanges(), or SaveChangesAsync() is called.
+            Console.WriteLine("Async Complete!");
+
+
+        }
+    }
+}
