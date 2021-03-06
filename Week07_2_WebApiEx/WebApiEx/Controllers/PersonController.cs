b37f346f09274ed2c52ac28f85e3717be640a86d
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2021-03-05 20:54:36 -0500

    Week07-02

---
+++ Week07_2_WebApiEx/WebApiEx/Controllers/PersonController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiEx.Data;
+using WebApiEx.Models;
+
+namespace WebApiEx.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly WebApiExContext _context;
+
+        public PersonController(WebApiExContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Person
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPerson()
+        {
+            return await _context.Person.ToListAsync();
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(Guid id)
+        {
+            var person = await _context.Person.FindAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+
+        // PUT: api/Person/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson(Guid id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Person
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Person>> PostPerson(Person person)
+        {
+            _context.Person.Add(person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new { id = person.Id }, person);
+        }
+
+        // DELETE: api/Person/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Person>> DeletePerson(Guid id)
+        {
+            var person = await _context.Person.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.Person.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return person;
+        }
+
+        private bool PersonExists(Guid id)
+        {
+            return _context.Person.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Week07_2_WebApiEx/WebApiEx/Data/WebApiExContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiEx.Models;
+
+namespace WebApiEx.Data
+{
+    public class WebApiExContext : DbContext
+    {
+        public WebApiExContext (DbContextOptions<WebApiExContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApiEx.Models.Person> Person { get; set; }
+    }
+}
